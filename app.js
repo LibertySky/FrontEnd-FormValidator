64ff33aab1588250d5499a60cbdf3ee9799d8074
LibertySky <hello@LibertySkygraphics.com>
2020-12-29 21:16:07 -0400

    refactored code

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ function showSuccess(input) {
 }
 
 // Check email is valid
-function isValidEmail(input) {
+function checkEmail(input) {
 	const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 	if (re.test(input.value.trim())) {
 		showSuccess(input);
@@ -28,20 +28,61 @@ function isValidEmail(input) {
 	}
 }
 
+//check required fields
+function checkRequired(inputArr) {
+	inputArr.forEach(function (input) {
+		if (input.value.trim() === '') {
+			showError(input, `${getFieldName(input)} is required`);
+		} else {
+			showSuccess(input);
+		}
+	});
+}
+
+//check input check length
+function checkLength(input, min, max) {
+	if (input.value.length < min) {
+		showError(
+			input,
+			`${getFieldName(input)} must be at least ${min} characters`
+		);
+	} else if (input.value.length > max) {
+		showError(
+			input,
+			`${getFieldName(input)} must be less then ${max} characters`
+		);
+	} else {
+		showSuccess(input);
+	}
+}
+
+//check passwords match
+function checkPasswordsMatch(input1, input2) {
+	if (input1.value !== input2.value) {
+		showError(input2, 'Passwords do not match');
+	} else {
+		showSuccess(input2);
+	}
+}
+
+function getFieldName(input) {
+	return input.id.charAt(0).toUpperCase() + input.id.slice(1);
+}
+
+form.addEventListener('focusout', (e) => {
+	checkRequired([username, email, password, password2]);
+	checkLength(username, 3, 15);
+	checkLength(password, 6, 25);
+	checkEmail(email);
+	checkPasswordsMatch(password, password2);
+});
+
 form.addEventListener('submit', (e) => {
 	e.preventDefault();
 
-	if (username.value === '') {
-		showError(username, 'Username is required');
-	} else {
-		showSuccess(username);
-	}
-
-	if (email.value === '') {
-		showError(email, 'Email is required');
-	} else if (!isValidEmail(email)) {
-		showError(email, 'Email is not valid');
-	} else {
-		showSuccess(email);
-	}
+	checkRequired([username, email, password, password2]);
+	checkLength(username, 3, 15);
+	checkLength(password, 6, 25);
+	checkEmail(email);
+	checkPasswordsMatch(password, password2);
 });
